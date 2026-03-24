ee2247b88495cc464b6552703e4d5dcf3d3d987b
Karthik-ai07 <ksakumar088@gmail.com>
2026-03-24 08:44:46 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   basePath: "/my_website",
-  assetPrefix: "/my_website/",
+  assetPrefix: "/my_website",
   images: {
     unoptimized: true,
   },
